343c73dad2756e8e33fb05dbb0ae5e44d626f717
iou90 kant <=>
2016-07-04 11:09:34 +0800

    edited assembly information

--- Kant.Wpf.Controls.Chart.SankeyDiagram/Kant.Wpf.Controls.Chart.SankeyDiagram/Properties/AssemblyInfo.cs
+++ Kant.Wpf.Controls.Chart.SankeyDiagram/Kant.Wpf.Controls.Chart.SankeyDiagram/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kant.Wpf.Controls.Chart.SankeyDiagram")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A powerful and easy to use WPF library for drawing Sankey diagram")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Kant.Wpf.Controls.Chart.SankeyDiagram")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
