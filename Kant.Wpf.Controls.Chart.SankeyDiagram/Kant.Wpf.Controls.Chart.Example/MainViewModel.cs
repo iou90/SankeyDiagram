e0a0a6ebfbe627736a229125d62aa49e53e31c87
iou90 kant <iou90@outlook.com>
2016-08-22 09:43:12 +0800

    minor fixes

--- Kant.Wpf.Controls.Chart.SankeyDiagram/Kant.Wpf.Controls.Chart.Example/MainViewModel.cs
+++ Kant.Wpf.Controls.Chart.SankeyDiagram/Kant.Wpf.Controls.Chart.Example/MainViewModel.cs
@@ -29,97 +29,97 @@ namespace Kant.Wpf.Controls.Chart.Example
             SankeyLabelStyle = sankeyLabelStyle2;
             SankeyToolTipTemplate = sankeyToolTipTemplate1;
 
+            //var datas = new List<SankeyData>()
+            //{
+            //    new SankeyData("Agricultural 'waste'", "Bio-conversion", 124.729),
+            //    new SankeyData("Bio-conversion","Liquid", 0.597),
+            //    new SankeyData("Bio-conversion", "Losses", 26.862),
+            //    new SankeyData("Bio-conversion", "Solid", 280.322),
+            //    new SankeyData("Bio-conversion", "Gas", 81.144),
+            //    new SankeyData("Biofuel imports", "Liquid", 35),
+            //    new SankeyData("Biomass imports", "Solid", 35),
+            //    new SankeyData("Coal imports", "Coal", 11.606),
+            //    new SankeyData("Coal reserves", "Coal", 63.965),
+            //    new SankeyData("Coal", "Solid", 75.571),
+            //    new SankeyData("District heating", "Industry", 10.639),
+            //    new SankeyData("District heating", "Heating and cooling - commercial", 22.505),
+            //    new SankeyData("District heating", "Heating and cooling - homes", 46.184),
+            //    new SankeyData("Electricity grid", "Over generation / exports", 104.453),
+            //    new SankeyData("Electricity grid", "Heating and cooling - homes", 113.726),
+            //    new SankeyData("Electricity grid", "H2 conversion", 27.14),
+            //    new SankeyData("Electricity grid", "Industry", 342.165),
+            //    new SankeyData("Electricity grid", "Road transport", 37.797),
+            //    new SankeyData("Electricity grid", "Agriculture", 4.412),
+            //    new SankeyData("Electricity grid", "Heating and cooling - commercial", 40.858),
+            //    new SankeyData("Electricity grid", "Losses", 56.691),
+            //    new SankeyData("Electricity grid", "Rail transport", 7.863),
+            //    new SankeyData("Electricity grid", "Lighting & appliances - commercial", 90.008),
+            //    new SankeyData("Electricity grid", "Lighting & appliances - homes", 93.494),
+            //    new SankeyData("Gas imports", "Ngas", 40.719),
+            //    new SankeyData("Gas reserves", "Ngas", 82.233),
+            //    new SankeyData("Gas", "Heating and cooling - commercial", 0.129),
+            //    new SankeyData("Gas", "Losses", 1.401),
+            //    new SankeyData("Gas", "Thermal generation", 151.891),
+            //    new SankeyData("Gas", "Agriculture", 2.096),
+            //    new SankeyData("Gas", "Industry", 48.58),
+            //    new SankeyData("Geothermal", "Electricity grid", 7.013),
+            //    new SankeyData("H2 conversion", "H2", 20.897),
+            //    new SankeyData("H2 conversion", "Losses", 6.242),
+            //    new SankeyData("H2", "Road transport", 20.897),
+            //    new SankeyData("Hydro", "Electricity grid", 6.995),
+            //    new SankeyData("Liquid", "Industry", 121.066),
+            //    new SankeyData("Liquid", "International shipping", 128.69),
+            //    new SankeyData("Liquid", "Road transport", 135.835),
+            //    new SankeyData("Liquid", "Domestic aviation", 14.458),
+            //    new SankeyData("Liquid", "International aviation", 206.267),
+            //    new SankeyData("Liquid", "Agriculture", 3.64),
+            //    new SankeyData("Liquid", "National navigation", 33.218),
+            //    new SankeyData("Liquid", "Rail transport", 4.413),
+            //    new SankeyData("Marine algae", "Bio-conversion", 4.375),
+            //    new SankeyData("Ngas", "Gas", 122.952),
+            //    new SankeyData("Nuclear", "Thermal generation", 839.978),
+            //    new SankeyData("Oil imports", "Oil", 504.287),
+            //    new SankeyData("Oil reserves", "Oil", 107.703),
+            //    new SankeyData("Oil", "Liquid", 611.99),
+            //    new SankeyData("Other waste", "Solid", 56.587),
+            //    new SankeyData("Other waste", "Bio-conversion", 77.81),
+            //    new SankeyData("Pumped heat",  "Heating and cooling - homes", 193.026),
+            //    new SankeyData("Pumped heat", "Heating and cooling - commercial", 70.672),
+            //    new SankeyData("Solar PV", "Electricity grid", 59.901),
+            //    new SankeyData("Solar Thermal", "Heating and cooling - homes", 19.263),
+            //    new SankeyData("Solar", "Solar Thermal", 19.263),
+            //    new SankeyData("Solar", "Solar PV", 59.901),
+            //    new SankeyData("Solid", "Agriculture", 0.882),
+            //    new SankeyData("Solid", "Thermal generation", 400.12),
+            //    new SankeyData("Solid", "Industry", 46.477),
+            //    new SankeyData("Thermal generation", "Electricity grid", 525.531),
+            //    new SankeyData("Thermal generation", "Losses", 787.129),
+            //    new SankeyData("Thermal generation", "District heating", 79.329),
+            //    new SankeyData("Tidal", "Electricity grid", 9.452),
+            //    new SankeyData("UK land based bioenergy", "Bio-conversion", 182.01),
+            //    new SankeyData("Wave", "Electricity grid", 19.013),
+            //    new SankeyData("Wind", "Electricity grid", 289.366)
+            //};
+
             var datas = new List<SankeyData>()
             {
-                new SankeyData("Agricultural 'waste'", "Bio-conversion", 124.729),
-                new SankeyData("Bio-conversion","Liquid", 0.597),
-                new SankeyData("Bio-conversion", "Losses", 26.862),
-                new SankeyData("Bio-conversion", "Solid", 280.322),
-                new SankeyData("Bio-conversion", "Gas", 81.144),
-                new SankeyData("Biofuel imports", "Liquid", 35),
-                new SankeyData("Biomass imports", "Solid", 35),
-                new SankeyData("Coal imports", "Coal", 11.606),
-                new SankeyData("Coal reserves", "Coal", 63.965),
-                new SankeyData("Coal", "Solid", 75.571),
-                new SankeyData("District heating", "Industry", 10.639),
-                new SankeyData("District heating", "Heating and cooling - commercial", 22.505),
-                new SankeyData("District heating", "Heating and cooling - homes", 46.184),
-                new SankeyData("Electricity grid", "Over generation / exports", 104.453),
-                new SankeyData("Electricity grid", "Heating and cooling - homes", 113.726),
-                new SankeyData("Electricity grid", "H2 conversion", 27.14),
-                new SankeyData("Electricity grid", "Industry", 342.165),
-                new SankeyData("Electricity grid", "Road transport", 37.797),
-                new SankeyData("Electricity grid", "Agriculture", 4.412),
-                new SankeyData("Electricity grid", "Heating and cooling - commercial", 40.858),
-                new SankeyData("Electricity grid", "Losses", 56.691),
-                new SankeyData("Electricity grid", "Rail transport", 7.863),
-                new SankeyData("Electricity grid", "Lighting & appliances - commercial", 90.008),
-                new SankeyData("Electricity grid", "Lighting & appliances - homes", 93.494),
-                new SankeyData("Gas imports", "Ngas", 40.719),
-                new SankeyData("Gas reserves", "Ngas", 82.233),
-                new SankeyData("Gas", "Heating and cooling - commercial", 0.129),
-                new SankeyData("Gas", "Losses", 1.401),
-                new SankeyData("Gas", "Thermal generation", 151.891),
-                new SankeyData("Gas", "Agriculture", 2.096),
-                new SankeyData("Gas", "Industry", 48.58),
-                new SankeyData("Geothermal", "Electricity grid", 7.013),
-                new SankeyData("H2 conversion", "H2", 20.897),
-                new SankeyData("H2 conversion", "Losses", 6.242),
-                new SankeyData("H2", "Road transport", 20.897),
-                new SankeyData("Hydro", "Electricity grid", 6.995),
-                new SankeyData("Liquid", "Industry", 121.066),
-                new SankeyData("Liquid", "International shipping", 128.69),
-                new SankeyData("Liquid", "Road transport", 135.835),
-                new SankeyData("Liquid", "Domestic aviation", 14.458),
-                new SankeyData("Liquid", "International aviation", 206.267),
-                new SankeyData("Liquid", "Agriculture", 3.64),
-                new SankeyData("Liquid", "National navigation", 33.218),
-                new SankeyData("Liquid", "Rail transport", 4.413),
-                new SankeyData("Marine algae", "Bio-conversion", 4.375),
-                new SankeyData("Ngas", "Gas", 122.952),
-                new SankeyData("Nuclear", "Thermal generation", 839.978),
-                new SankeyData("Oil imports", "Oil", 504.287),
-                new SankeyData("Oil reserves", "Oil", 107.703),
-                new SankeyData("Oil", "Liquid", 611.99),
-                new SankeyData("Other waste", "Solid", 56.587),
-                new SankeyData("Other waste", "Bio-conversion", 77.81),
-                new SankeyData("Pumped heat",  "Heating and cooling - homes", 193.026),
-                new SankeyData("Pumped heat", "Heating and cooling - commercial", 70.672),
-                new SankeyData("Solar PV", "Electricity grid", 59.901),
-                new SankeyData("Solar Thermal", "Heating and cooling - homes", 19.263),
-                new SankeyData("Solar", "Solar Thermal", 19.263),
-                new SankeyData("Solar", "Solar PV", 59.901),
-                new SankeyData("Solid", "Agriculture", 0.882),
-                new SankeyData("Solid", "Thermal generation", 400.12),
-                new SankeyData("Solid", "Industry", 46.477),
-                new SankeyData("Thermal generation", "Electricity grid", 525.531),
-                new SankeyData("Thermal generation", "Losses", 787.129),
-                new SankeyData("Thermal generation", "District heating", 79.329),
-                new SankeyData("Tidal", "Electricity grid", 9.452),
-                new SankeyData("UK land based bioenergy", "Bio-conversion", 182.01),
-                new SankeyData("Wave", "Electricity grid", 19.013),
-                new SankeyData("Wind", "Electricity grid", 289.366)
+                new SankeyData("A", "C", 255),
+                new SankeyData("A", "D", 355),
+                new SankeyData("B", "C", 555),
+                new SankeyData("B", "D", 255),
+                new SankeyData("B", "E", 1555),
+                new SankeyData("C", "H", 155),
+                new SankeyData("D", "F", 25),
+                new SankeyData("D", "G", 155),
+                new SankeyData("D", "H", 15),
+                new SankeyData("D", "I", 55),
+                new SankeyData("E", "H", 1555),
+                new SankeyData("B", "G", 255),
+                new SankeyData("A", "E", 95),
+                new SankeyData("E", "I", 1555),
+                new SankeyData("C", "G", 755),
+                new SankeyData("C", "F", 455),
             };
-
-            //var datas = new List<SankeyDataRow>()
-            //{
-            //    new SankeyDataRow("A", "C", 255),
-            //    new SankeyDataRow("A", "D", 355),
-            //    new SankeyDataRow("B", "C", 555),
-            //    new SankeyDataRow("B", "D", 255),
-            //    new SankeyDataRow("B", "E", 1555),
-            //    new SankeyDataRow("C", "H", 155),
-            //    new SankeyDataRow("D", "F", 25),
-            //    new SankeyDataRow("D", "G", 155),
-            //    new SankeyDataRow("D", "H", 15),
-            //    new SankeyDataRow("D", "I", 55),
-            //    new SankeyDataRow("E", "H", 1555),
-            //    new SankeyDataRow("B", "G", 255),
-            //    new SankeyDataRow("A", "E", 95),
-            //    new SankeyDataRow("E", "I", 1555),
-            //    new SankeyDataRow("C", "G", 755),
-            //    new SankeyDataRow("C", "F", 455),
-            //};
 
             SankeyDatas = datas;
             SankeyShowLabels = true;
@@ -289,17 +289,17 @@ namespace Kant.Wpf.Controls.Chart.Example
         }
 
         private Dictionary<string, Brush> sankeyNodeBrushes;
-        public Dictionary<string, Brush> SankeyNodeBrushes
+        public IReadOnlyDictionary<string, Brush> SankeyNodeBrushes
         {
             get
             {
                 return sankeyNodeBrushes;
             }
-            set
+            private set
             {
                 if(value != sankeyNodeBrushes)
                 {
-                    sankeyNodeBrushes = value;
+                    sankeyNodeBrushes = value == null ? null : value.ToDictionary(item => item.Key, item => item.Value);
                     RaisePropertyChanged(() => SankeyNodeBrushes);
                 }
             }
